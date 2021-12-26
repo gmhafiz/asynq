58d990c1cc84d61589c96af04fc4af529a4b1b2a
Hafiz Shafruddin <gmhafiz@gmail.com>
2021-12-26 19:53:30 +1100

    clean up render

--- internal/utility/respond/render.go
+++ internal/utility/respond/render.go
@@ -2,10 +2,18 @@ package respond
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"log"
 	"net/http"
-	"reflect"
+)
+
+var (
+	ErrBadRequest    = errors.New("bad request")
+	ErrNoRecord      = errors.New("no record found")
+	ErrInternalError = errors.New("internal")
+
+	ErrDatabase       = errors.New("connecting to database")
+	ErrInvalidRequest = errors.New("invalid request")
 )
 
 func Render(w http.ResponseWriter, statusCode int, payload interface{}) {
@@ -28,46 +36,51 @@ func Render(w http.ResponseWriter, statusCode int, payload interface{}) {
 	}
 }
 
-func Error(w http.ResponseWriter, statusCode int, payload interface{}) {
+func Errors(w http.ResponseWriter, statusCode int, errors []string) {
 	w.WriteHeader(statusCode)
-	if payload == nil {
-		_, err := w.Write(nil)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		if reflect.ValueOf(payload).Type().String() == "*errors.errorString" {
-			err := fmt.Errorf("%v", payload)
-			p := map[string]string{
-				"error": err.Error(),
-			}
-			data, err := json.Marshal(p)
-			if err != nil {
-				log.Println(err)
-			}
-			write(w, data)
-		} else if reflect.TypeOf(payload).Kind() == reflect.Slice {
-			var errors []string
-			s := reflect.ValueOf(payload)
-			for i := 0; i < s.Len(); i++ {
-				errors = append(errors, s.Index(i).String())
-			}
-			p := map[string][]string{
-				"error": errors,
-			}
-			data, err := json.Marshal(p)
-			if err != nil {
-				log.Println(err)
-			}
-			write(w, data)
-		} else {
-			data, err := json.Marshal(payload)
-			if err != nil {
-				log.Println(err)
-			}
-			write(w, data)
-		}
+
+	if errors == nil {
+		write(w, nil)
+		return
 	}
+
+	p := map[string][]string{
+		"message": errors,
+	}
+	data, err := json.Marshal(p)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if string(data) == "null" {
+		return
+	}
+
+	write(w, data)
+}
+
+func Error(w http.ResponseWriter, statusCode int, message error) {
+	w.WriteHeader(statusCode)
+
+	var p map[string]string
+	if message == nil {
+		write(w, nil)
+		return
+	}
+
+	p = map[string]string{
+		"message": message.Error(),
+	}
+	data, err := json.Marshal(p)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if string(data) == "null" {
+		return
+	}
+
+	write(w, data)
 }
 
 func write(w http.ResponseWriter, data []byte) {
